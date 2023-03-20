48904c4dddee0c31d5c7c0ac5341dde89715db6e
Rebeca Soares <98848463+rebecasoares982@users.noreply.github.com>
2023-03-20 17:12:19 -0300

    update

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@
 })(jQuery);
 
 
+const btns = document.querySelectorAll('.toggle-forms .btn');
+		const forms = document.querySelectorAll('.forms-container .form');
+
+		btns.forEach(btn => {
+			btn.addEventListener('click', () => {
+				// Remove a classe "active" de todos os botões e formulários
+				btns.forEach(btn => btn.classList.remove('active'));
+				forms.forEach(form => form.classList.remove('active'));
+				// Adiciona a classe "active" ao botão e formulário selecionados
+				const formId = btn.getAttribute('data-form');
+				const form = document.querySelector(`#${formId}`);
+				btn.classList.add('active');
+				form.classList.add('active');
+			});
+		});
